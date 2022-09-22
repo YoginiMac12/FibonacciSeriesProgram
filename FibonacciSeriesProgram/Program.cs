4cf441c190305e9b201f91b54eb7404c9c4daacd
YoginiMac12 <113415806+YoginiMac12@users.noreply.github.com>
2022-09-22 23:09:15 +0530

    Add files via upload

---
+++ FibonacciSeriesProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeriesProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Fibonacci Number:");
+            int n = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine();
+            int a = 0;
+            int b = 1;
+            int c;
+ 
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            for (int i = 2; i < n; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+            Console.ReadLine();    
+        }
+    }
+}
